0a88ef5c0ed5d19ab8e4fcd0ace6e486ae68e815
Frank Staals <frank@Hina.local>
2013-05-21 20:00:54 +0200

    moved the c implementaions to a separate file

---
+++ src/Network/LibRSync/internal.c
@@ -0,0 +1,74 @@
+#include "internal.h"
+#include "librsync.h"
+#include <stdio.h>
+
+rs_result genSig(char* filePath, int fd) {
+    FILE*      f;
+    FILE*      sigFile;
+    rs_stats_t stats;
+    rs_result  result;
+
+    f       = fopen(filePath, "rb");
+    sigFile = fdopen(fd, "wb");
+
+    result = rs_sig_file(f, sigFile,
+                         RS_DEFAULT_BLOCK_LEN, RS_DEFAULT_STRONG_LEN, &stats);
+    rs_file_close(f);
+
+    // Note that we leave the sigfile open
+
+    return result;
+}
+
+
+// generate a delta, based on the implementation of rdiff
+rs_result genDelta(int sigFd, char* filePath, int deltaFd) {
+    FILE*           sigFile;
+    FILE*           f;
+    FILE*           deltaFile;
+    rs_result       result;
+    rs_signature_t* sumset;
+    rs_stats_t      stats;
+
+    sigFile   = fdopen(sigFd, "rb");
+    f         = fopen(filePath, "rb");
+    deltaFile = fdopen(deltaFd, "wb");
+
+    result = rs_loadsig_file(sigFile, &sumset, &stats);
+    if (result != RS_DONE)
+       return result;
+
+    if ((result = rs_build_hash_table(sumset)) != RS_DONE)
+       return result;
+
+    result = rs_delta_file(sumset, f, deltaFile, &stats);
+
+    rs_free_sumset(sumset);
+
+    // rs_file_close(deltaFile);
+    rs_file_close(f);
+    rs_file_close(sigFile);
+
+    return result;
+}
+
+
+rs_result applyPatch(int deltaFd, char* inputPath, char* outputPath) {
+    FILE*      deltaFile;
+    FILE*      inputFile;
+    FILE*      outputFile;
+    rs_stats_t stats;
+    rs_result  result;
+
+    inputFile  = fopen(inputPath, "rb");
+    deltaFile  = fdopen(deltaFd, "rb");
+    outputFile = fopen(outputPath, "wb");
+
+    result = rs_patch_file(inputFile, deltaFile, outputFile, &stats);
+
+    rs_file_close(inputFile);
+    // keep the delta file open
+    rs_file_close(outputFile);
+
+    return result;
+}
