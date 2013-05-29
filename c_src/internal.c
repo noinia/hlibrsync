9ebf18bbd6220f1387aedb4e2a103dde3f87b0de
Frank Staals <frank@Hina.local>
2013-05-29 20:25:13 +0200

    We are converting stuff to conduits on the haskell side. This is the C-code to compute signatures that goes along with it

--- c_src/internal.c
+++ c_src/internal.c
@@ -5,7 +5,7 @@
 #include <assert.h>
 
 #include "internal.h"
-#include "buf.h"
+//#include "buf.h"
 
 // -----------------------------------------------------------------------------
 // Debugging functions
@@ -41,176 +41,138 @@ void printOut(inMemoryBuffer_t *output) {
 // -----------------------------------------------------------------------------
 // The acutal functiosn
 
-/*
- * Function to write the signature into a `bytestream': an array of bytes.
- * When we call this function we expect to get:
- *   - a pointer to a job : job,
- *   - a pointer to an rs_buffers struct: buf, and
- *   - a pointer to a inMemoryBuffer_t struct: opaque, the buffer+meta
- *              info about the buffer  where to write to
- *
- * Note that opaque is a void pointer to satisfy the type signature of the callback.
- *
- * We will set up buf to *directly* write the output stream, i.e. sigBuf->sig.
- * this means this function has two tasks: (1) initialize this properly, and (2)
- * if we run low on space in sigBuf->sig, allocate more space.
- *
- */
-rs_result outputSignature(rs_job_t *job, rs_buffers_t *buf, void *opaque) {
-    char *newBuffer;
-    inMemoryBuffer_t *output = (inMemoryBuffer_t*) opaque;
+rs_result signatureCb(rs_job_t *job, rs_buffers_t *buf, void *opaque) {
+    rsyncSourceState_t *state = (rsyncSourceState_t*) opaque;
 
-    /* This is only allowed if either the buf has no output buffer
-     * yet, or that buffer could possibly be BUF. */
+    assert(state != NULL);
+
+    inMemoryBuffer_t *output = state->outputBuf;
+
+    assert(output != NULL);
+
+    // initialize the buf
     if (buf->next_out == NULL) {
         assert(buf->avail_out == 0);
 
         // allow us to directly write into the output stream
         buf->next_out = output->buffer;
         buf->avail_out = output->size;
+        output->inUse = 0;
 
         return RS_DONE;
     }
 
-    // If we no longer have space left, double the storage space.
+
+    output->inUse = output->size - buf->avail_out;
+
     if (buf->avail_out == 0) {
-
-        printf("ran out of space");
-        printOut(output);
-
-        newBuffer = (char*) malloc(2*(output->size));
-        memcpy(newBuffer,output->buffer, output->size);
-
-        free(output->buffer);
-
-        output->buffer = newBuffer;
-        buf->avail_out = output->size;
-        buf->next_out  = output->buffer + output->size;
-
-        output->size *= 2;
-
-        printf("output after enlarging buffer\n");
-        printOut(output);
-
-
-        return RS_DONE;
+        return RS_BLOCKED;
     }
 
-    assert(buf->avail_out > 0);
-
-    // that is it
 
     return RS_DONE;
 }
 
 
-rs_result genSig(char *filePath, inMemoryBuffer_t *output) {
+rs_result startSignature(char *filePath, rsyncSourceState_t *state) {
 
-    FILE *f;
-    rs_job_t *job;
     rs_buffers_t buf;
-    rs_filebuf_t *inBuf = NULL;
-    rs_result result;
 
-    if (output->buffer == NULL) {
-        output->buffer = malloc(DEFAULT_BUFFERSIZE);
-        output->size = DEFAULT_BUFFERSIZE;
+    if (state->outputBuf == NULL) {
+        state->outputBuf = malloc(sizeof(inMemoryBuffer_t));
+        state->outputBuf->buffer = malloc(DEFAULT_BUFFERSIZE);
+        state->outputBuf->size = DEFAULT_BUFFERSIZE;
+        state->outputBuf->inUse = 0;
     }
 
-    output->inUse = 0;
+    inMemoryBuffer_t *output = state->outputBuf;
 
-    f = fopen(filePath, "rb");
-    if (!f)
+    /* printf("setting up buf\n"); */
+
+    /* // Set up so the job uses output->buffer as buffer */
+    /* buf->next_out = output->buffer; */
+    /* buf->avail_out = output->size; */
+
+    state->f = fopen(filePath, "rb");
+    if (!state->f)
         return RS_IO_ERROR;
 
-    inBuf  = rs_filebuf_new(f, rs_inbuflen);
+    state->inBuf  = rs_filebuf_new(state->f, rs_inbuflen);
 
-    job = rs_sig_begin(RS_DEFAULT_BLOCK_LEN, RS_DEFAULT_STRONG_LEN);
+    state->job = rs_sig_begin(RS_DEFAULT_BLOCK_LEN, RS_DEFAULT_STRONG_LEN);
 
-    result = rs_job_drive(job, &buf,
-                          inBuf  ? rs_infilebuf_fill : NULL, inBuf,
-                          outputSignature, output
-                          );
+    return  rs_job_drive(state->job, &buf,
+                         state->inBuf  ? rs_infilebuf_fill : NULL, state->inBuf,
+                         signatureCb, state
+                         );
+}
 
-    if (inBuf)
-        rs_filebuf_free(inBuf);
+void endSignature(rsyncSourceState_t *state) {
 
-    // compute the final size of the signature
-    output->inUse = output->size - buf.avail_out;
 
     printf ("Generated the signature:\n");
-    printOut(output);
+    printOut(state->outputBuf);
     printf("DONE printing\n\n\n");
 
-    rs_job_free(job);
-    fclose(f);
+    fclose(state->f);
+    rs_job_free(state->job);
+    if (state->inBuf)
+        rs_filebuf_free(state->inBuf);
+    free(state->outputBuf->buffer);
+    free(state->outputBuf);
+}
 
-    return result;
+rs_result signatureChunk(rsyncSourceState_t *state) {
+    // set up the output buffers
+    assert(state != NULL);
+    assert(state->job != NULL);
+    assert(state->inBuf != NULL);
+    assert(state->outputBuf != NULL);
+    assert(state->outputBuf->buffer != NULL);
+
+    rs_buffers_t buf;
+
+    return rs_job_drive(state->job, &buf,
+                         state->inBuf  ? rs_infilebuf_fill : NULL, state->inBuf,
+                         signatureCb, state
+                         );
 }
 
 
-// generate a delta, based on the implementation of rdiff
-rs_result genDelta(int sigFd, char* filePath, int deltaFd) {
-    FILE*           sigFile;
-    FILE*           f;
-    FILE*           deltaFile;
-    rs_result       result;
-    rs_signature_t* sumset;
 
-    sigFile   = fdopen(sigFd, "rb");
-    f         = fopen(filePath, "rb");
-    deltaFile = fdopen(deltaFd, "wb");
-
-    result = rs_loadsig_file(sigFile, &sumset, NULL);
-    if (result != RS_DONE)
-       return result;
-
-    if ((result = rs_build_hash_table(sumset)) != RS_DONE)
-       return result;
-
-    result = rs_delta_file(sumset, f, deltaFile, NULL);
-
-    rs_free_sumset(sumset);
-
-    fclose(deltaFile);
-    fclose(f);
-    fclose(sigFile);
-
-    return result;
-}
-
-
-rs_result applyPatch(int deltaFd, char* inputPath, char* outputPath) {
-    FILE*      deltaFile;
-    FILE*      inputFile;
-    FILE*      outputFile;
-    rs_result  result;
-
-    inputFile  = fopen(inputPath, "rb");
-    deltaFile  = fdopen(deltaFd, "rb");
-    outputFile = fopen(outputPath, "wb");
-
-    result = rs_patch_file(inputFile, deltaFile, outputFile, NULL);
-
-    fclose(inputFile);
-    fclose(deltaFile);
-    fclose(outputFile);
-
-    return result;
-}
 
 // -----------------------------------------------------------------------------
 // For testing purposes
 
 int main(int argc, char *argv[]) {
-    inMemoryBuffer_t *sig = (inMemoryBuffer_t*) malloc(sizeof(inMemoryBuffer_t));
 
-    rs_result result = genSig("/Users/frank/tmp/httpd-error.log",sig);
+    int i;
+    rs_result result;
 
-    printOut(sig);
-    toFile("/tmp/signature",sig->buffer,sig->inUse);
+    rsyncSourceState_t *state = malloc(sizeof(rsyncSourceState_t));
 
-    free(sig);
+    printf ("Starting up \n");
+    result= startSignature("/Users/frank/tmp/httpd-error.log", state);
+
+    printf ("Started :)\n");
+    printf("initial data:\n");
+    printOut(state->outputBuf);
+
+
+    for (i = 0; i < 6; ++i) {
+        printf ("Getting a chunk");
+        assert(state != NULL);
+        assert(state->job != NULL);
+        assert(state->inBuf != NULL);
+        assert(state->outputBuf != NULL);
+        assert(state->outputBuf->buffer != NULL);
+        printf("..\n");
+        result = signatureChunk(state);
+        printOut(state->outputBuf);
+    }
+
+    endSignature(state);
+    free(state);
 
     return 0;
 }
--- c_src/internal.h
+++ c_src/internal.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <librsync.h>
 
+#include "buf.h"
+
+
+// typedef rs_filebuf rs_filebuf_t;
 
 typedef struct inMemoryBuffer_t {
     char *buffer;
@@ -8,16 +12,24 @@ typedef struct inMemoryBuffer_t {
     size_t inUse;
 } inMemoryBuffer_t;
 
-size_t DEFAULT_BUFFERSIZE = 16;
+typedef struct rsyncSourceState_t {
+    FILE *f;
+    rs_job_t *job;
+    rs_filebuf_t *inBuf;
+    inMemoryBuffer_t *outputBuf;
+} rsyncSourceState_t;
 
-// we will produce the output in output. If output->buffer = NULL
-// we will allocate it ourselves using DEFAULT_SIZE.
-// Note: DONT keep pointers directly to output->buffer, because they
-// may become invalid !!!!
-rs_result genSig(char *filePath, inMemoryBuffer_t *output);
+
+size_t DEFAULT_BUFFERSIZE = 6;
+
+/* // we will produce the output in output. If output->buffer = NULL */
+/* // we will allocate it ourselves using DEFAULT_SIZE. */
+/* // Note: DONT keep pointers directly to output->buffer, because they */
+/* // may become invalid !!!! */
+/* rs_result genSig(char *filePath, inMemoryBuffer_t *output); */
 
 
-// generate a delta, based on the implementation of rdiff
-rs_result genDelta(int sigFd, char* filePath, int deltaFd);
+/* // generate a delta, based on the implementation of rdiff */
+/* rs_result genDelta(int sigFd, char* filePath, int deltaFd); */
 
-rs_result applyPatch(int deltaFd, char* inputPath, char* outputPath);
+/* rs_result applyPatch(int deltaFd, char* inputPath, char* outputPath); */
