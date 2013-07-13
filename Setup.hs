896c07d753ebf1cdbe360117d689b0c4cdcdf97d
Frank Staals <frank@Hina.local>
2013-07-13 19:03:28 +0200

    compiling per cabal works now

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,18 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils (rawSystemExit)
+
+main = defaultMainWithHooks simpleUserHooks
+    { preBuild = \a b -> makeLib a b >> preBuild simpleUserHooks a b }
+
+makeLib         :: Args -> BuildFlags -> IO ()
+makeLib _ flags = do
+    print "Compiling C Helper functions"
+    rawSystemExit (fromFlag $ buildVerbosity flags) "env"
+        ["CFLAGS=-D_LIB", "make", "--directory=c_src", "all"]
+
+
+
+
+-- import Distribution.Simple
+-- main = defaultMain
